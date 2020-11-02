d160db75c3a3498cdbb19f861d6bd584da81c4bc
sdixitdhinakaran <73838549+sdixitdhinakaran@users.noreply.github.com>
2020-11-02 19:16:27 +0000

    Update contact_form.php

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Contact form <email@sitp.ac.in>';
-$sendTo = 'shekhar@sitp.ac.in';
+$sendTo = 'sdixitdhinakaran@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
